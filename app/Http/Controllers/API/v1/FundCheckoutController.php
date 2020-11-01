de464cd7e0fbccf813c0618177328c032a1502e3
Maulana Ichwan Anshory <64744726+masiwang@users.noreply.github.com>
2020-11-01 16:05:14 +0700

    Update FundCheckoutController.php

--- app/Http/Controllers/API/v1/FundCheckoutController.php
+++ app/Http/Controllers/API/v1/FundCheckoutController.php
@@ -15,7 +15,7 @@ class FundCheckoutController extends Controller
 {
     public function portofolio(Request $request){
         $portofolios = FundCheckout::where('user_id', Auth::id());
-        if($request->container == 'profile_page'){
+        if($request->container){
             $portofolios = $portofolios->whereNotNull('pay_at')
                 ->limit(2);
         }else{
